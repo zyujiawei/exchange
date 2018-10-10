ecfaa7e4e063fa97ab51ca393054befcff95a94d
arthur <zyujiawei@hotmail.com>
2018-10-10 21:56:13 +0800

    load data on both table

--- public/javascripts/helper.js
+++ public/javascripts/helper.js
@@ -40,9 +40,127 @@ function getTree(){
   return data
 }
 
+function statusFormatter(value, row, index, field){
+  var text = 'error';
+  console.log(value)
+  switch (value) {
+    case 0,1:
+      text = '未成交'
+      break;
+    case 4,5:
+      text = '取消'
+      break;
+    case 3:
+      text = '部分成交'
+      break;
+    default:
+      text = '完全完成'
+      break;
+  }
+  return text;
+}
+
 $(document).ready(function(){
+  $('#onholdtable').bootstrapTable({
+    columns: [{
+        field: 'cointype',
+        title: '币种'
+    }, {
+        field: 'buy',
+        title: '总买'
+    }, {
+        field: 'sell',
+        title: '总卖'
+    }, {
+        field: 'hold',
+        title: '持仓'
+    }]
+  });
+  $('#oredertable').bootstrapTable({
+    columns: [{
+        field: 'side',
+        title: '买卖方向'
+    }, {
+        field: 'price',
+        title: '价格'
+    }, {
+        field: 'volume',
+        title: '数量'
+    }, {
+        field: 'avg_price',
+        title: '平均价格'
+    }, {
+        field: 'deal_volume',
+        title: '已成交数量'
+    }, {
+        field: 'status',
+        title: '订单状态',
+        formatter: function (value, row, index, field){
+          var text = 'error';
+          console.log(value)
+          switch (value) {
+            case 0,1:
+              text = '未成交'
+              break;
+            case 4,5:
+              text = '取消'
+              break;
+            case 3:
+              text = '部分成交'
+              break;
+            default:
+              text = '完全完成'
+              break;
+          }
+          return text;
+        }
+    }, {
+        field: 'ctime',
+        title: '委托时间',
+        sortable: true,
+        order: 'desc',
+    }, {
+        field: 'mtime',
+        title: '处理时间'
+    }]
+  });
   $('#tree').treeview({
       data: getTree()
   });
   $('#tree').treeview('expandAll', { silent: true });
+  $('#tree').on('nodeSelected', function(event, data) {
+    var id = data.id;
+    var cointype = 'cvt';
+    var todaydate = new Date();
+    var end_date = todaydate.getFullYear()+"-" + (todaydate.getMonth()+1) + "-" + todaydate.getDate();
+    $.ajax({
+      url:"/api/order/"+id+"/cargo/"+cointype+"/"+end_date,
+      type:'get',
+      async:false,
+      success:function(result){
+        result.cointype = 'CVT';
+        $('#onholdtable').bootstrapTable('load',[result]);
+      }
+    });
+  });
+  $('#queryButton').click(function(){
+    var selectedNode = $('#tree').treeview('getSelected');
+    if (selectedNode.length > 0){
+      var cointype = 'cvtbtc';
+      var start_date = '2008-01-02';
+      var end_date = '2018-01-01';
+      var id = selectedNode[0].id;
+      $.ajax({
+        url:"/api/order/"+id+"/"+cointype+"/"+start_date+"/"+end_date,
+        type:'get',
+        async:false,
+        success:function(result){
+          $('#oredertable').bootstrapTable('load',result);
+        }
+      });
+    } else{
+      alert('请选择下线用户');
+    }
+
+  })
 })
--- routes/api.js
+++ routes/api.js
@@ -17,13 +17,49 @@ router.get('/branch', function(req, res, next) {
 );
 });
 
-router.get('/order', function(req, res, next) {
-  res.json([{"user_id":11230,"side":"SELL","price":0.1000000000000000,"volume":2625.0000000000000000,"avg_price":0.1000000000000000,"deal_volume":2625.0000000000000000,"status":2,"ctime":"2018-09-16T23:58:06+08:00","mtime":"2018-09-16T23:59:59+08:00"},{"user_id":11230,"side":"BUY","price":0.0450000000000000,"volume":5500.0000000000000000,"avg_price":0.0450000000000000,"deal_volume":5500.0000000000000000,"status":2,"ctime":"2018-09-17T00:02:59+08:00","mtime":"2018-09-17T02:43:05+08:00"}]);
+router.get('/order/:id/cargo/:cointype/:end_date', function(req, res, next) {
+  var id = req.params.id;
+  var data;
+  switch(id){
+    case '13552': data={"buy":5500.0,"sell":8125.0,"hold":-2625.0};
+    break;
+    case '13434': data={"buy":5600.0,"sell":8125.0,"hold":-2525.0};
+    break;
+    case '13435': data={"buy":5700.0,"sell":8125.0,"hold":-2425.0};
+    break;
+    case '13553': data={"buy":5800.0,"sell":8125.0,"hold":-2325.0};
+    break;
+    case '13436': data={"buy":5900.0,"sell":8125.0,"hold":-2225.0};
+    break;
+  }
+  res.json(data);
 });
 
-router.get('/cargo', function(req, res, next) {
-  res.json({"buy":5500.0,"sell":8125.0,"hold":-2625.0});
+router.get('/order/:id/:cointype/:start_date/:end_date', function(req, res, next) {
+  var id = req.params.id;
+  var data;
+  switch(id){
+    case '13552': data=[{"user_id":13552,"side":"SELL","price":0.1000000000000000,"volume":2625.0000000000000000,"avg_price":0.1000000000000000,"deal_volume":2625.0000000000000000,"status":2,"ctime":"2018-09-16T23:58:06+08:00","mtime":"2018-09-16T23:59:59+08:00"},
+      {"user_id":13552,"side":"BUY","price":0.0450000000000000,"volume":5500.0000000000000000,"avg_price":0.0450000000000000,"deal_volume":5500.0000000000000000,"status":1,"ctime":"2018-09-15T00:02:59+08:00","mtime":"2018-09-17T02:43:05+08:00"},
+    {"user_id":13552,"side":"BUY","price":0.0450000000000000,"volume":5500.0000000000000000,"avg_price":0.0450000000000000,"deal_volume":5500.0000000000000000,"status":5,"ctime":"2018-09-17T00:02:59+08:00","mtime":"2018-09-17T02:43:05+08:00"}];
+    break;
+    case '13434': data=[{"user_id":13434,"side":"SELL","price":0.2000000000000000,"volume":2625.0000000000000000,"avg_price":0.1000000000000000,"deal_volume":2625.0000000000000000,"status":2,"ctime":"2018-09-16T23:58:06+08:00","mtime":"2018-09-16T23:59:59+08:00"},
+    {"user_id":13434,"side":"BUY","price":0.0450000000000000,"volume":5500.0000000000000000,"avg_price":0.0450000000000000,"deal_volume":5500.0000000000000000,"status":2,"ctime":"2018-09-17T00:02:59+08:00","mtime":"2018-09-17T02:43:05+08:00"}];
+    break;
+    case '13435': data=[{"user_id":13435,"side":"SELL","price":0.3000000000000000,"volume":2625.0000000000000000,"avg_price":0.1000000000000000,"deal_volume":2625.0000000000000000,"status":2,"ctime":"2018-09-16T23:58:06+08:00","mtime":"2018-09-16T23:59:59+08:00"},
+    {"user_id":13435,"side":"BUY","price":0.0450000000000000,"volume":5500.0000000000000000,"avg_price":0.0450000000000000,"deal_volume":5500.0000000000000000,"status":2,"ctime":"2018-09-17T00:02:59+08:00","mtime":"2018-09-17T02:43:05+08:00"}];
+    break;
+    case '13553': data=[{"user_id":13553,"side":"SELL","price":0.4000000000000000,"volume":2625.0000000000000000,"avg_price":0.1000000000000000,"deal_volume":2625.0000000000000000,"status":2,"ctime":"2018-09-16T23:58:06+08:00","mtime":"2018-09-16T23:59:59+08:00"},
+    {"user_id":13553,"side":"BUY","price":0.0450000000000000,"volume":5500.0000000000000000,"avg_price":0.0450000000000000,"deal_volume":5500.0000000000000000,"status":2,"ctime":"2018-09-17T00:02:59+08:00","mtime":"2018-09-17T02:43:05+08:00"}];
+    break;
+    case '13436': data=[{"user_id":13436,"side":"SELL","price":0.5000000000000000,"volume":2625.0000000000000000,"avg_price":0.1000000000000000,"deal_volume":2625.0000000000000000,"status":2,"ctime":"2018-09-16T23:58:06+08:00","mtime":"2018-09-16T23:59:59+08:00"},
+    {"user_id":13436,"side":"BUY","price":0.0450000000000000,"volume":5500.0000000000000000,"avg_price":0.0450000000000000,"deal_volume":5500.0000000000000000,"status":2,"ctime":"2018-09-17T00:02:59+08:00","mtime":"2018-09-17T02:43:05+08:00"}];
+    break;
+  }
+  res.json(data);
 });
+
+
 
 
 module.exports = router;
